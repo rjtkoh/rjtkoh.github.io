977ad3fbcda3e9dc4b2903f270b1958c93f0ab17
rjtkoh <rjtkoh@gmail.com>
2020-08-04 13:00:34 +1000

    added copy to clipboard

--- scripts.js
+++ scripts.js
@@ -1,6 +1,25 @@
-function applySelected(){
-    if(location.href='index.html'){
-      document.write('hello')
-    }
-  }
-  
+
+function copyStuff(){
+  document.getElementById("btnCopy");
+  document.execCommand("Copy");
+  document.querySelector(".tooltip").setAttribute("id", "tooltipcopied");
+  document.querySelector(".triangle").setAttribute("id", "trianglecopied");
+  document.getElementById("tooltipcopied").innerHTML = "Copied!";
+}
+
+
+
+function enterFunc() {
+  document.querySelector(".tooltip").setAttribute("id", "tooltipshow");
+  document.querySelector(".triangle").setAttribute("id", "tooltipshow");
+}
+
+
+
+function outFunc() {
+ 
+   document.querySelector(".tooltip").setAttribute("id", "tooltiphidden");
+  document.querySelector(".tooltip").innerHTML = "Copy to clipboard";
+  document.querySelector(".triangle").setAttribute("id", "tooltiphidden");
+
+}
