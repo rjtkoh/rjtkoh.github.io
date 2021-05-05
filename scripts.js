2ba003cc863b36383aeabff1ff84cfd99bd93da0
precisionbusiness <precisionbusiness888@gmail.com>
2021-05-05 13:44:52 +1000

    fdsf

--- scripts.js
+++ scripts.js
@@ -46,3 +46,37 @@ window.addEventListener('scroll', function(e){
     // Update the old value
     oldValue = newValue;
 });
+
+
+// SIDE BAR FIX
+
+window.addEventListener('scroll', function (){
+  const sideContent = document.querySelector('.third');
+  const titleShrink = document.querySelector('h1');
+  const projectDesc = document.querySelector('h3');
+  const cvSection = document.querySelector('.cv-wrapper');
+  var currentY= window.pageYOffset;
+  var cvY = cvSection.offsetTop;
+  
+    console.log('currentY '+ currentY);
+    console.log('cv scroll '+ cvY);
+   
+  
+    if(currentY > 230 && currentY < (cvY-520)) {
+       sideContent.setAttribute("id", "sidecontentAlt");
+       titleShrink.setAttribute("id", "h1Alt");
+       projectDesc.style.display = "none";
+       sideContent.style.display = "block";
+      } 
+      else if (currentY < 230 && currentY < (cvY-520)) {
+        sideContent.removeAttribute("id", "sidecontentAlt");
+        titleShrink.removeAttribute("id", "h1Alt");
+        projectDesc.style.display = "block";
+        sideContent.style.display = "block";
+      } 
+      else if (currentY > (cvY-4520)){
+          sideContent.style.display = "none";
+      }
+  
+    }    
+  )
---
+++ sidebar.js
@@ -0,0 +1,35 @@
+
+window.addEventListener('scroll', function (){
+const sideContent = document.querySelector('.third');
+const titleShrink = document.querySelector('h1');
+const projectDesc = document.querySelector('h3');
+const cvSection = document.querySelector('.cv-wrapper');
+var currentY= window.pageYOffset;
+var cvY = cvSection.offsetTop;
+
+  console.log('currentY '+ currentY);
+  console.log('cv scroll '+ cvY);
+ 
+
+  if(currentY > 230 && currentY < (cvY-500)) {
+     sideContent.setAttribute("id", "sidecontentAlt");
+     titleShrink.setAttribute("id", "h1Alt");
+     projectDesc.style.display = "none";
+     sideContent.style.display = "block";
+    } 
+    else if (currentY < 230 && currentY < (cvY-500)) {
+      sideContent.removeAttribute("id", "sidecontentAlt");
+      titleShrink.removeAttribute("id", "h1Alt");
+      projectDesc.style.display = "block";
+      sideContent.style.display = "block";
+    } 
+    else if (currentY > (cvY-400)){
+        sideContent.style.display = "none";
+    }
+
+  }    
+)
+
+
+
+
